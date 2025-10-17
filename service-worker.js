0c166f415953a6db4f02949b7d561525e7c0d51e
couple-wishlist <fauzanmargito@gmail.com>
2025-10-17 08:38:57 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,46 @@
-const CACHE_NAME = 'nabung-v1';
-const ASSETS = [
-  '/', '/index.html', '/style.css', '/app.js',
-  '/icon-192.png', '/icon-512.png', '/manifest.json'
+const CACHE_NAME = "tabungan-app-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./manifest.json"
 ];
 
-self.addEventListener('install', e=>{
+// Instal Service Worker dan cache file penting
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
   self.skipWaiting();
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(()=>{}));
 });
-self.addEventListener('activate', e=>{
-  e.waitUntil(self.clients.claim());
+
+// Aktifkan SW dan hapus cache lama
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
 });
-self.addEventListener('fetch', e=>{
-  const req = e.request;
-  e.respondWith(caches.match(req).then(r=> r || fetch(req).then(resp=>{
-    // update cache for GET
-    if (req.method === 'GET') {
-      caches.open(CACHE_NAME).then(cache=>cache.put(req, resp.clone()));
-    }
-    return resp;
-  }).catch(()=> caches.match('/index.html'))));
+
+// Tangani request jaringan
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return (
+        response ||
+        fetch(event.request).catch(() => caches.match("./index.html"))
+      );
+    })
+  );
 });
